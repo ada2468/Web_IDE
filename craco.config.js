88112208f8339327bf6812e2c98d79e2bd33e47d
ada2468 <eachinabnw168@hotmail.com>
2022-03-09 00:38:11 -0500

    add docs

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const path = require('path');
 
-  };
+module.exports = {
+   webpack: {
+        configure: (webpackConfig, { env, paths }) => { 
+            paths.appBuild = webpackConfig.output.path = path.resolve('./docs');
+            return webpackConfig;  // Important: return the modified config
+        }
+    }
+}
